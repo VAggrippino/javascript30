26d404e36c32e5f1c070852fe90239ba35666065
Vince Aggrippino <yhqktpi@gmail.com>
2017-02-07 22:51:05 +0800

    Minor bug fixes to CSS & HTML.

--- 29_CountdownClock/main.js
+++ 29_CountdownClock/main.js
@@ -1,5 +1,3 @@
-console.clear();
-
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
