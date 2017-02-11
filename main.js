01f86b246ca068929240d214d4d46b8b32c7cc97
Vince Aggrippino <yhqktpi@gmail.com>
2017-02-11 23:40:49 +0800

    Add details for exercises up to 30.

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 $(function() {
   const videos = document.querySelectorAll('.video');
   const titles = document.querySelectorAll('.title');
+  const projects = document.querySelectorAll('.project');
 
   videos.forEach(video => {
     const player = video.querySelector('video');
-    video.addEventListener('mouseover', () => player.play());
-    video.addEventListener('mouseout', () => player.pause());
+    video.addEventListener('mouseover', () => {
+      player.play();
+      video.classList.add('playing');
+    });
+    video.addEventListener('mouseout', () => {
+      player.pause();
+      video.classList.remove('playing');
+    });
   });
 
   titles.forEach(title => {
     if (title.offsetWidth > title.parentNode.offsetWidth) {
       title.classList.add('scrolling');
     }
-  })
+  });
+
+  projects.forEach(project => {
+    project.addEventListener('mouseover', () => project.classList.add('mouseover'));
+    project.addEventListener('mouseleave', () => project.classList.remove('mouseover'));
+  });
 });
